dc987938e38a381533530493e37db605997d1522
Chris Edwards <cedwards@reisystems.com>
2016-04-22 16:33:05 -0400

    lets use express

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-const Server = require('aliv');
+var express = require('express');
+var app = express();
+var path = require('path');
 
-new Server({quiet: true, port: process.env.PORT || 3000, secure: true, pathIndex: "client/dev", only: "client/dev/**/*"}).start();
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/client/dev'));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/client/dev/index.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
